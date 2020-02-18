eeec8e0f0a42fe49b7d3ac5c3cbc4f219e4dbe2c
Aggropelzer <marius.gerhard@edu.bib.de>
2020-02-18 16:58:17 +0000

    Header Title Legal Notice

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {HeaderTitleService} from '../services/header-title.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(private headerTitleService: HeaderTitleService) { }
+
+  ngOnInit() {
+    this.headerTitleService.setTitle('Profile');
+  }
+
+}
